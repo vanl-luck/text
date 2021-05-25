6331d74a39196bba1590e7fa9a017afd02a91481
vant <414599861@qq.com>
2021-05-25 10:46:01 +0800

    修改bug

--- src/view/main/tabs/TabProducts.vue
+++ src/view/main/tabs/TabProducts.vue
@@ -9,10 +9,10 @@
     <div class="img-responsive">
     </div>
     <div>
-      <div v-for="(item,index) in vip" :key="index" style="color:#69696a" class="borderCard">
+      <div v-for="(item,index) in zsImg" :key="index" style="color:#69696a" class="borderCard">
         <!-- <van-card > -->
         <div class="tags">
-          <img :src="item" alt="" style="width: 185px;height:220px">
+          <img :src="item.img" alt="" style="width: 185px;height:220px">
         </div>
         <div style="text-align: center;width:100%;color:#ababab">
           <div style="height:100px;" v-if="index==0">
@@ -30,7 +30,7 @@
           </div>
           <div>
 
-            <van-button type="primary" style="width: 125px;" @click="getList(index)">点击加入</van-button>
+            <van-button v-if="item.state==0" type="primary" style="width: 125px;" @click="getList(item.id)">点击加入</van-button>
           </div>
         </div>
 
@@ -70,39 +70,9 @@
       </div>
 
     </div>
-    <!-- <div class="imgBackground" style="">
-      </div> -->
 
-    <!-- </span>
 
-    <!-- <div class="top-fixed">
-      <van-nav-bar :title="$t('tab_products')" fixed :z-index="10"></van-nav-bar>
-      <div class="nav-con navbar-con">
-        <van-tabs v-model="activeSort" class="sort-all" sticky @change="onTabChange" line-width="33%">
-          <van-tab :title="it_so.title" v-for="(it_so, idx) in sortItems" :key="idx">
-            <div class="prod-item click-box" v-for="(it_po,idx_p) in getProductList(idx)"
-                 :key="idx_p" @click="onProductClick(it_po)">
-              <van-row>
-                <van-col span="12" class="prod-rate">
-                  <div class="red-txt rate-txt"><span class="rate-big">{{it_po.rate}}</span>%+{{it_po.add}}%</div>
-                  <div class="light-txt rate-ins">预期年化收益率</div>
-                </van-col>
-                <van-col span="12" class="prod-ins">
-                  <div class="prod-title">{{it_po.name}}</div>
-                  <div class="prod-tag">期限{{it_po.days}}天</div>
-                </van-col>
-              </van-row>
-              <van-progress :percentage="it_po.percent" color="#ff6611" class="prod-progress" :show-pivot="false">
-              </van-progress>
-              <div class="pro-amount">
-                <div>项目总额180万</div>
-                <div>剩余可投<span class="red-txt">75万</span></div>
-              </div>
-            </div>
-          </van-tab>
-        </van-tabs>
-      </div>
-    </div> -->
+ 
   </div>
 </template>
 
@@ -145,11 +115,23 @@
         ],
         allProducts: [],
         vip: [
-          
-          '../../../../static/img/vip/vip.jpeg', '../../../../static/img/vip/WechatIMG2143.jpeg',
-          '../../../../static/img/vip/私懂.jpeg'
+
+          {
+            img: '../../../../static/img/vip/980.png',
+            id: 3
+          },
+          {
+            img: '../../../../static/img/vip/10000.png',
+            id: 4
+          },
+          {
+            img: '../../../../static/img/vip/30000.png',
+            id: 5
+          },
+
         ],
-payInfo:{}
+        zsImg:[],
+        payInfo: {}
       };
     },
     computed: {
@@ -172,19 +154,35 @@ payInfo:{}
     },
     created() {
 
+      this.getMemberInfo()
       this.clearLoc()
     },
     methods: {
-      getList(index){
-        let mebId=index
-      let phone=JSON.parse(localStorage.getItem("user")).phone
-     this.$http.get(`api/wx/pay/app?type=1&phone=${phone}&memberId=3`).then(res => {
-     
-        this.payInfo=res.data
-        this.wexinPay(res.data.data)
-      }).catch(err => {
-        this._showToast('系统错误');
-      });
+      getMemberInfo() {
+        const url = '/api/user/query'
+        const data = {
+          userId: JSON.parse(localStorage.getItem('user')).id
+        }
+        this.$http.post(url, data).then(res => {
+          if(res.data.code==200){
+            res.data.data.forEach((item,index) => {
+              item.img=this.vip[index].img
+              return item
+            });
+              this.zsImg=res.data.data
+              console.log(this.zsImg);
+          }
+        })
+      },
+      getList(index) {
+        let phone = JSON.parse(localStorage.getItem("user")).phone
+        this.$http.get(`api/wx/pay/app?type=1&phone=${phone}&memberId=${index}`).then(res => {
+
+          this.payInfo = res.data
+          this.wexinPay(res.data.data)
+        }).catch(err => {
+          this._showToast('系统错误');
+        });
       },
       clearLoc() {
         this.$http.get(
@@ -196,195 +194,25 @@ payInfo:{}
         })
       },
 
-    async  wexinPay(index) {
-     
-console.log(      'getBrandWCPayRequest', {
-         "appId":index.appId,     //公众号名称，由商户传入     
-         "timeStamp":index.timeStamp,         //时间戳，自1970年以来的秒数     
-         "nonceStr":index.nonceStr, //随机串     
-         "package":index.package,     
-         "signType":index.signType,         //微信签名方式：     
-         "paySign":index.sign //微信签名 
-      },);
+      async wexinPay(index) {
+        WeixinJSBridge.invoke(
+          'getBrandWCPayRequest', {
+            "appId": index.appId, //公众号名称，由商户传入     
+            "timeStamp": index.timeStamp, //时间戳，自1970年以来的秒数     
+            "nonceStr": index.nonceStr, //随机串     
+            "package": index.package,
+            "signType": index.signType, //微信签名方式：     
+            "paySign": index.sign //微信签名 
+          },
+          function (res) {
+            if (res.err_msg == "get_brand_wcpay_request:ok") {
+              // 使用以上方式判断前端返回,微信团队郑重提示：
+              //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+            }
+          });
 
-           WeixinJSBridge.invoke(
-      'getBrandWCPayRequest', {
-         "appId":index.appId,     //公众号名称，由商户传入     
-         "timeStamp":index.timeStamp,         //时间戳，自1970年以来的秒数     
-         "nonceStr":index.nonceStr, //随机串     
-         "package":index.package,     
-         "signType":index.signType,         //微信签名方式：     
-         "paySign":index.sign //微信签名 
       },
-      function(res){
-      if(res.err_msg == "get_brand_wcpay_request:ok" ){
-      // 使用以上方式判断前端返回,微信团队郑重提示：
-            //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-      } 
-   }); 
-        // const data={
 
-        //   appid: 'wx27c08fe4a23c5aa5',
-        //   mchid: '1609680603',
-        //   description: '花果山医美',
-        //   out_trade_no: 'H51217752501201407033233368018',
-        //   notify_url: 'http://fun75s.natappfree.cc/api/wx/pay/notify',
-        //   amount: {
-        //     total: 0.01
-        //   },
-        //   scene_info: {
-        //     "payer_client_ip": "159.75.232.235",
-        //     "h5_info": {
-        //       "type": "Wap"
-        //     }
-        //   }
-        
-        // }
-        // const url='/api.mch.weixin.qq.com/v3/pay/transactions/h5'
-        // this.$http.post(url,data).then(res=>{
-        //   console.log(res);
-        // })
-        // this.$http.post('/api.mch.weixin.qq.com/v3/pay/transactions/h5').then(res=>{
-        //   console.log(res);
-        // }))
-
-        //获取后台传入的数据
-        // let appId = data.appId;
-        // let timestamp = data.timeStamp;
-        // let nonceStr = data.nonceStr;
-        // let signature = data.signature;
-        // let packages = data.package;
-        // let paySign = data.paySign;
-        // let signType = data.signType;
-        // console.log('发起微信支付')
-        // //下面要发起微信支付了
-        // wx.config({
-        //   debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-        //   appId: appId, // 必填，公众号的唯一标识
-        //   timestamp: timestamp, // 必填，生成签名的时间戳
-        //   nonceStr: nonceStr, // 必填，生成签名的随机串
-        //   signature: signature, // 必填，签名，见附录1
-        //   jsApiList: ['chooseWXPay'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-        // });
-        // wx.ready(function () {
-        //   wx.chooseWXPay({
-        //     timestamp: timestamp, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-        //     nonceStr: nonceStr, // 支付签名随机串，不长于 32 位
-        //     package: packages, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-        //     signType: signType, // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-        //     paySign: paySign, // 支付签名
-        //     success: function (res) {
-        //       // 支付成功后的回调函数
-        //       cb(res);
-        //     },
-        //     fail: function (res) {
-        //       //失败回调函数
-        //       errorCb(res);
-        //     }
-        //   });
-        // });
-        // wx.error(function (res) {
-        //   // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-        //   /*alert("config信息验证失败");*/
-        // });
-      },
-      pay(value) {
-        //h5微信支付
-        // this.$
-
-
-        // if (JSON.parse(localStorage.getItem('user')).payState == 1) {
-        //   this._showToast('已缴费');
-        // } else {
-        //   this.$router.push({
-        //     name: 'test',
-        //     query: value,
-        //   });
-        // }
-        // console.log(value);
-        // this.$router.push('test')
-
-
-
-        //  const BASE_URL = 'http://192.168.0.108:7878/zkview/';
-
-
-        // this.$http.get(`api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx4cf67da399a7cad0&secret=62e2bcacaffa86f92ce69f30a8ace141`).then(res=>{
-        //   console.log(ers);
-        // })
-
-        // https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx520c15f417810387&redirect_uri=https%3A%2F%2Fchong.qq.com%2Fphp%2Findex.php%3Fd%3D%26c%3DwxAdapter%26m%3DmobileDeal%26showwxpaytitle%3D1%26vb2ctag%3D4_2030_5_1194_60&response_type=code&scope=snsapi_base&state=123#wechat_redirect
-
-
-
-        // window.location.href = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4cf67da399a7cad0&redirect_uri=http://http://www.anmeihui.cn/#/main/products&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
-        // return
-
-        //                this.axios.post("user/recommend", data).then(res => {
-        //   //成功状态下 
-        //   if (res.data.status) {
-        //    // 存储微信支付数据data
-        //    let data = res.data.data;
-        //    console.log("即将跳转微信支付");
-        //    //函数为分装过得 (就是调用微信支付)
-        //    http.wexinPay(
-        //    {
-        //     appId: data.appId,
-        //     nonceStr: data.nonceStr,
-        //     package: data.package,
-        //     paySign: data.paySign,
-        //     signType: data.signType,
-        //     timeStamp: data.timeStamp
-        //    },
-        //    //成功回调函数
-        //    res => {
-        //     that.$router.push({ path: "/vip" });
-        //    }
-        //    );
-        //   } 
-        //   else {
-        //    that.$message.error(res.data.message);
-        //  }
-        // });
-        // return
-        // var payParams = {
-        //   // "appId": $wxData.appId,
-        //   // "timeStamp": $wxData.timeStamp, //时间戳，自1970年以来的秒数
-        //   // "nonceStr": $wxData.nonceStr, //随机串
-        //   // "package": $wxData.package,
-        //   // "signType": $wxData.signType, //微信签名方式：
-        //   // "paySign": $wxData.paySign //微信签名
-        // };
-        // var req = localStorage.getItem("payParams");
-        // if (!req) return;
-        // else req = JSON.parse(req);
-        // WeixinJSBridge.invoke(
-        //   'getBrandWCPayRequest', req,
-        //   function (res) {
-        //     if (res.err_msg == "get_brand_wcpay_request:ok") {
-        //       // alert("支付成功");
-        //     } // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回 ok，但并不保证它绝对可靠。
-        //     else {
-        //       console.log(res.err_msg);
-
-        //       //支付失败后的操作
-        //       window.location.href = "pay.html?ticketPrice=" + $tickprice + "&actid=" + $actid + "&ticketId=" +
-        //         $ticketId + "&sellerid=" + $sellerid;
-        //     }
-        //   }
-
-        // );
-        // if (typeof WeixinJSBridge == "undefined") {
-        //   if (document.addEventListener) {
-        //     document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
-        //   } else if (document.attachEvent) {
-        //     document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
-        //     document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
-        //   }
-        // } else {
-        //   onBridgeReady();
-        // }
-      },
 
       onProductClick(item) {
         this._routePushQ('ProductDetail', {
